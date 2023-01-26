148f29251e079923bc6ebab75d58dcb6ec3747bd
jeanvier <damien.grossfeld@gmail.com>
2023-01-26 15:34:40 +0100

    fix: wrong scrip load

--- templates/Admin/Pages/manage.php
+++ templates/Admin/Pages/manage.php
@@ -1,12 +1,5 @@
-<?
-$this->Html->css([
-  'pages/main.min.css?v='.md5_file(APP.'../webroot/css/pages/main.min.css')
-],['block' => 'css']);
-// js
+<?php
 $this->Html->scriptBlock("window.troisPage = ".json_encode($page).";", ['block' => true]);
-$this->Html->script([
-  'pages/main.min.js?v='.md5_file(APP.'../webroot/js/pages/main.min.js'),
-],['block' => true]);
 ?>
 <nav class="navbar navbar-expand-lg">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
